6b88906faafb9915cbb375170c030181b5d80ea2
gustavoronan <gustavoronn@gmail.com>
2025-02-14 14:09:08 -0300

    add: UserService class and methods

---
+++ src/main/java/com/project/course/services/UserService.java
@@ -0,0 +1,29 @@
+package com.project.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.project.course.entities.User;
+import com.project.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		
+		return userRepository.findAll();	
+		
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.get();
+	}
+}
+
