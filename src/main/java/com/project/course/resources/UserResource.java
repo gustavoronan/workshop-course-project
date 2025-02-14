b11e69c93881e3208d3af03f77c87ccd63510aa8
gustavoronan <gustavoronn@gmail.com>
2025-02-14 14:07:50 -0300

    feat: get user by id implemented

--- src/main/java/com/project/course/resources/UserResource.java
+++ src/main/java/com/project/course/resources/UserResource.java
@@ -1,18 +1,35 @@
 package com.project.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.course.entities.User;
+import com.project.course.services.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User user = new User(1L,"Maria", "maria@gmail.com","999999", "12345");
-		return ResponseEntity.ok().body(user);
+	public ResponseEntity<List<User>> findAll(){
+		
+		List<User> listaUsuario = userService.findAll();
+		return ResponseEntity.ok().body(listaUsuario);
+	}
+	
+	@GetMapping("/usuario/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		
+		User userObj = userService.findById(id);
+		return ResponseEntity.ok().body(userObj);
 	}
 }
