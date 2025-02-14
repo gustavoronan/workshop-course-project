72013650d68baf6be0107c226d2dc783c18e248f
gustavoronan <gustavoronn@gmail.com>
2025-02-14 10:27:30 -0300

    working...

--- src/main/java/com/project/course/repositories/UserRepository.java
+++ src/main/java/com/project/course/repositories/UserRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.project.course.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
+	 
 	
 }
